dc494fe16f0c588f0120905acd4ec0505ac59b49
ysahannah <hleguro8@gmail.com>
2024-10-21 14:52:39 +0800

    update

--- src/About.tsx
+++ src/About.tsx
@@ -7,7 +7,7 @@ function About() {
                 <h2 className="text-light">ABOUT ME</h2>
             </center>
             <center>
-                <div className="main">
+                <div className="main mt-4">
                     <p className="text-light about-me">
                         Hi there! I’m Hannah Ysabelle Leguro, and I’m excited to share my journey with you. As a Virtual Assistant, I have a love for technology and a background in administrative tasks, tech support, and web development. I enjoy helping others by managing client inquiries, creating engaging presentations, and lending a hand on web projects.
                         I’m all about problem-solving and finding ways to make things easier for everyone. As I continue to learn and grow, I can’t wait to contribute my skills in a supportive role while exploring new adventures in the tech world. Thanks for stopping by my portfolio—I’m glad you’re here!
