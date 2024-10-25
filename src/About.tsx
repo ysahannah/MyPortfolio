f8685d82ee9a4261afa8cdd1161b24e0ad631bdf
ysahannah <hleguro8@gmail.com>
2024-10-26 02:04:01 +0800

    update the About.tsx and About.css

--- src/About.tsx
+++ src/About.tsx
@@ -6,14 +6,14 @@ function About() {
         <div id="About" className="about">
             <div className="d-flex flex-row about-container">
                 <div className="about-image">
-                    <img src={meImage} alt="Hannah's Image" /> {/* Use the imported image */}
+                    <img src={meImage} alt="Hannah's Image" />
                 </div>
                 <div className="about-description me-5">
                     <center>
                         <h2 className="about-title">ABOUT ME</h2>
-                        <h5 className="about-me">
+                        <h6 className="about-me">
                             I'm an IT graduate passionate about problem-solving, troubleshooting, and web development. As a virtual assistant, I’ve honed my skills in various technical and administrative roles, and I'm eager to contribute and grow in a supportive environment. Let’s build solutions together!
-                        </h5>
+                        </h6>
                     </center>
                 </div>
             </div>
