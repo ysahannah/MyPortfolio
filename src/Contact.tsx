3e57cf7d458d25b8b14cd3df0f3416f767d57c7b
ysahannah <hleguro8@gmail.com>
2024-10-25 11:09:03 +0800

    update

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -11,7 +11,7 @@ function Contact() {
         message: ''
     });
 
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
         setFormData({
             ...formData,
@@ -19,7 +19,7 @@ function Contact() {
         });
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('Form Submitted', formData);
     };
@@ -32,72 +32,75 @@ function Contact() {
                         <h2 id="Contact" className="text-light">CONTACT ME</h2>
                     </center>
                     
-                <div className="contact-info mt-3">
-                    <div className="row">
-                        <div className="col">
-                        <p className="email"><FontAwesomeIcon icon={faEnvelope} /> hleguro8@gmail.com</p>
+                    <div className="contact-info mt-3">
+                        <div className="row">
+                            <div className="col">
+                                <p className="email">
+                                    <FontAwesomeIcon icon={faEnvelope} /> hleguro8@gmail.com
+                                </p>
+                            </div>
+
+                            <div className="col">
+                                <p className="number">
+                                    <FontAwesomeIcon icon={faPhone} /> +639199313717
+                                </p>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="col">
-                        <p className="number">
-                            <FontAwesomeIcon icon={faPhone} />+639199313717</p>
+                    <div className="form-group mt-3">
+                        <label htmlFor="name">Name</label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                            required
+                            placeholder="Enter your name"
+                        />
                     </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
+                            required
+                            placeholder="Enter your email"
+                        />
                     </div>
-                </div>
-
-                <div className="form-group mt-3">
-                    <label htmlFor="name">Name</label>
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        value={formData.name}
-                        onChange={handleChange}
-                        required
-                        placeholder="Enter your name"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                        placeholder="Enter your email"
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="subject">Subject</label>
-                    <input 
-                    type="text" 
-                    id='subject'
-                    name='subject'
-                    value={formData.subject}
-                    onChange={handleChange}
-                    required
-                    placeholder='Write your subject here'
-                    />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea
-                        id="message"
-                        name="message"
-                        value={formData.message}
-                        onChange={handleChange}
-                        required
-                        placeholder="Write your message here"
-                    />
-                </div>
-                <div className="form-group">
-                    <center>
-                        <button type="submit" className="submit-btn">Send Message</button>
-                    </center>
-                </div>
-            </form>
+                    <div className="form-group">
+                        <label htmlFor="subject">Subject</label>
+                        <input 
+                            type="text" 
+                            id='subject'
+                            name='subject'
+                            value={formData.subject}
+                            onChange={handleChange}
+                            required
+                            placeholder='Write your subject here'
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message</label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            required
+                            placeholder="Write your message here"
+                        />
+                    </div>
+                    <div className="form-group">
+                        <center>
+                            <button type="submit" className="submit-btn">Send Message</button>
+                        </center>
+                    </div>
+                </form>
             </div>
         </div>
     );
