52948bbf7767f7f0b3071c1abf40d272397bcc6b
ysahannah <hleguro8@gmail.com>
2025-02-23 15:24:38 -0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,29 @@
+import logo1 from '../assets/yllogo.png';
+import '../styles/Footer.css';
+
 function Footer() {
-    return(
-        <footer>
+    return (
+        <footer className="footer">
             <center>
-                <h5>&copy; {new Date().getFullYear()} YZAVHEL</h5>
+                <div className="logo-footer">
+                    <img src={logo1} height="60px" alt="Logo" />
+                </div>
+
+                <div className="contact-container">
+                    <a href="mailto:hleguro8@gmail.com" target="_blank" rel="noopener noreferrer" className="contact-item">
+                        <i className="bi bi-envelope"></i>
+                        <span>hleguro8@gmail.com</span>
+                    </a>
+                    <a href="tel:+639398118600" target="_blank" rel="noopener noreferrer" className="contact-item">
+                        <i className="bi bi-telephone"></i>
+                        <span>+639199313717</span>
+                    </a>
+                </div>
+
+                <p className="copyright-text">&copy; {new Date().getFullYear()} YZAVHEL</p>
             </center>
         </footer>
     );
-    
 }
 
-export default Footer
+export default Footer;
